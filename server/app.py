8bceba6da828aa15342bb36ae7b5a01021c8c920
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-06-04 18:10:32 -0400

    done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -20,8 +20,47 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    
+    if earthquake:
+        body = {'id': earthquake.id,
+                'location': earthquake.location,
+                'magnitude': earthquake.magnitude,
+                'year': earthquake.year
+                }
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+        
+    return make_response(body, status)
 
+# @app.route('/earthquakes//magnitude/<float:magnitude>')
+# def earthquake_by_magnitude(magnitude):
+#     earthquakes = []
+    
+#     for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+#         earthquake_dict = {
+#             'id': earthquake.id,
+#             'location': earthquake.location,
+#             'magnitude': earthquake.magnitude,
+#             'year': earthquake.year
+#         }
+#         earthquakes.append(earthquake_dict)
+        
+#         body = {
+#         'count': len(earthquakes),
+#         'quakes': earthquakes
+#     }
+#         return jsonify(body),200
+    
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    earthquakes_data = [quake.to_dict() for quake in earthquakes]
+    return jsonify({"count": len(earthquakes_data), "quakes": earthquakes_data}), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
